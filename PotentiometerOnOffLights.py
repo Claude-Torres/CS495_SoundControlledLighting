0f32f1fa7a52754066e1d5c769ab985ddbfef6ed
Claude Torres <98055163+Claude-Torres@users.noreply.github.com>
2023-03-10 09:15:23 -0700

    added comments to PotentiometerOnOffLights.py

--- PotentiometerOnOffLights.py
+++ PotentiometerOnOffLights.py
@@ -1,9 +1,11 @@
 import RPi.GPIO as GPIO
 import threading
 
+# make global variable to check
 global manDecThresh
 manDecThresh = 1
 
+# Thread to turn on and off lights based on manDecThresh
 class LEDThread(threading.Thread):
     def __init__(self):
         super(LEDThread, self).__init__()
@@ -16,7 +18,7 @@ class LEDThread(threading.Thread):
         
         while True:
             # light LED's
-
+            # curremntly a constant .5 decibal input
             if manDecThresh > .5:
                 GPIO.output(light, GPIO.HIGH)
             elif manDecThresh <= .5:
@@ -26,6 +28,7 @@ class LEDThread(threading.Thread):
     def stop(self):
         self.stopped = True
 
+# thread to check potentiomerter status 
 class Potenti(threading.Thread):
     def __init__(self):
         super(Potenti, self).__init__()
@@ -43,19 +46,23 @@ class Potenti(threading.Thread):
     def stop(self):
         self.stopped = True
         
-
+# set up GPIO interface
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
+# label each part's pins
 power = 2
 potentiometer = 12
 light = 18
 
+# set sig as input for gpio
 GPIO.setup(potentiometer, GPIO.IN)
 
+# set light to on and output
 GPIO.setup(light, GPIO.OUT)
 GPIO.output(light, GPIO.HIGH)
 
+# start 1 of each thread
 potenti = Potenti()
 potenti.start()
 
